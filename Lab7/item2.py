a22d3f46812650c0475ed46dc4a14328437112ba
Sunnysunsun <88470765+64010459@users.noreply.github.com>
2022-10-18 22:03:56 +0700

    Add files via upload

---
+++ Lab7/item2.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class BST:
+    def __init__(self, root = None):
+        self.root = None
+
+    def insert(self,data):
+        if self.root is None:
+            self.root = Node(data)
+            return self.root
+        else: 
+            t = self.root
+            while True:
+                if data < t.data:
+                    if t.left == None:
+                        t.left = Node(data)
+                        return self.root
+                    else:
+                        t = t.left
+                else:
+                    if t.right == None:
+                        t.right = Node(data)
+                        return self.root
+                    else:
+                        t = t.right
+
+    def height(self,node):
+        if node is not None:
+            return max(self.height(node.left),self.height(node.right))+1
+        else:
+            return -1
+       
+
+T = BST()
+inp = [int(i) for i in input('Enter Input : ').split()]
+for i in inp:
+    root = T.insert(i)
+print("Height of this tree is :",T.height(T.root))
---
+++ Lab7/item3.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class BST:
+    def __init__(self, root = None):
+        self.root = None
+
+    def insert(self,data):
+        if self.root is None:
+            self.root = Node(data)
+            return self.root
+        else: 
+            t = self.root
+            while True:
+                if data <= t.data:
+                    if t.left == None:
+                        t.left = Node(data)
+                        return self.root
+                    else:
+                        t = t.left
+                else:
+                    if t.right == None:
+                        t.right = Node(data)
+                        return self.root
+                    else:
+                        t = t.right
+
+    def lessThan(self,node,k,count = 0):
+        if node != None:
+            if node.data <= k:
+                count = 1
+
+            count += self.lessThan(node.right,k)
+            count += self.lessThan(node.left,k)
+        return count 
+
+    def printTree(self, node, level = 0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+T = BST()
+inp,k = input('Enter Input : ').split("/")
+inp = [int(i) for i in inp.split()]
+for i in inp:
+    root = T.insert(i)
+T.printTree(root)
+print('--------------------------------------------------')
+print(T.lessThan(root,int(k)))
