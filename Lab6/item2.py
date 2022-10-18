c458a6cc0bcd38f095789f29cc6c78333fe4afbf
Sunnysunsun <88470765+64010459@users.noreply.github.com>
2022-10-18 22:17:22 +0700

    Update item2.py

--- Lab6/item2.py
+++ Lab6/item2.py
@@ -1,3 +1,8 @@
+#Length of a String EXTRA
+#ให้นักศึกษาเขียนฟังก์ชันที่ทำงานเหมือนกับฟังก์ชัน len() เพื่อหาความยาวของ string และแสดงผลดังตัวอย่าง(print ตัวอักษรตามด้วยเครื่องหมายพิเศษสลับกันคู่คี่)
+#****ห้ามใช้คำสั่ง len, for, while, do while, split*****
+# หมายเหตุ ฟังก์ชันต้องมี parameter แค่เพียง 1 ตัว
+
 def length(txt):
     def loop(lst, mode):
         if not lst:
